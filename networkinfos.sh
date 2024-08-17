e050ed8bf31303a648aa614336c9f59ca302c44a
MathieuAudibert <mathieu.audibert27@gmail.com>
2024-08-17 03:09:48 +0200

    ca marche!!

--- networkinfos.sh
+++ networkinfos.sh
@@ -1,7 +1,9 @@
-#!/bin/bashget_continent() {
+#!/bin/bash
+get_continent() {
     ip=$1
-    continent=$(curl -s https://ipinfo.io/$ip | grep country | awk -F '"''{print $4}')
-    case"$continent"in"AS")
+    continent=$(curl -s https://ipinfo.io/$ip | grep country | awk -F '"' '{print $4}')
+    case "$continent" in
+        "AS")
             continent_name="Asia"
             ;;
         "EU")
@@ -25,21 +27,42 @@
         *)
             continent_name="Unknown"
             ;;
-    esacecho$continent_name
+    esac
+    echo $continent_name
 }
 
 scan_network() {
-    echo"Scanning the network..."
-    nmap -sn 192.168.0.0/24 > results.txt
+    echo "Scanning the network..."
+    nmap -sn 192.168.0.0/24
 }
 
 display_info() {
-    echo"Network infos"echo -e "Made by MathieuA              \\             / /"echo -e "                          \\\\\\', ,      / //"echo -e "                           \\\\\\//,   _/ //,"echo -e "                            \\_-//' /  //<,"echo -e "                              \\ ///  >  \\\`__/_"echo -e "                              /,)-^>> _\` \\\\"echo -e "                              (/   \\\\ //\\\\"echo -e "                                  // _//\\\\\\\\"echo -e "                                ((\` (( "echo"--------------------"whileread -r line; doif [[ $line == *"Nmap scan report for"* ]]; then
-            ip=$(echo$line | awk '{print $5}')
-            ip_type="IPv4"if [[ $ip == *":"* ]]; then
-                ip_type="IPv6"fi
+    echo "Network infos"
+    echo -e "Made by MathieuA              \\             / /"
+    echo -e "                          \\\\\\', ,      / //"
+    echo -e "                           \\\\\\//,   _/ //,"
+    echo -e "                            \\_-//' /  //<,"
+    echo -e "                              \\ ///  >  \\\`__/_"
+    echo -e "                              /,)-^>> _\` \\\\"
+    echo -e "                              (/   \\\\ //\\\\"
+    echo -e "                                  // _//\\\\\\"
+    echo -e "                                ((\` (( "
+    echo "--------------------"
+    while read -r line; do
+        if [[ $line == *"Nmap scan report for"* ]]; then
+            ip=$(echo $line | awk '{print $5}')
+            ip_type="IPv4"
+            if [[ $ip == *":"* ]]; then
+                ip_type="IPv6"
+            fi
             continent=$(get_continent $ip)
-            echo"Network: 192.168.x.x"echo"IP: $ip"echo"IP Type: $ip_type"echo"Continent: $continent"echo"--------------------"fidone < results.txt
+            echo "Network: 192.168.x.x"
+            echo "IP: $ip"
+            echo "IP Type: $ip_type"
+            echo "Continent: $continent"
+            echo "--------------------"
+        fi
+    done
 }
 
 scan_network
